5ea8dfd4a784769c7146910f57d2731de1789c6f
trips4 <dennis.triplo@perforce.comm>
2025-11-10 15:52:16 -0600

    added base profile to manage a linux user

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,4 +1,43 @@
-# The base profile should include component modules that will be on all nodes
-class profile::base {
-
+#
+# @Summary Base profile class to be included in all roles.
+#
+# @param username
+#  The name of the user to manage.
+#
+# @param password
+#  The password for the user (Sensitive).
+#
+class profile::base (
+  String $username            = 'test_user',
+  Sensitive[String] $password = "MyPassword123!",
+) {
+  case $facts['kernel'] {
+    'Linux': {
+      user { $username:
+        ensure     => present,
+        password   => $password,
+        shell      => '/bin/bash',
+        managehome => true,
+        groups     => ['sudo'],
+      }
+      file_line { 'Enable PasswordAuthentication in SSH Dameon config':
+        ensure => present,
+        #path   => '/etc/ssh/sshd_config',
+        path   => '/etc/ssh/sshd_config.d/60-cloudimg-settings.conf',
+        line   => 'PasswordAuthentication yes',
+        match  => '^PasswordAuthentication',
+        notify => Service['sshd'],
+      }
+      service { 'sshd':
+        ensure => running,
+        enable => true,
+      }
+    }
+    'windows': {
+      # Additional Linux-specific configurations can be added here
+    }
+    default: {
+      fail("Unsupported kernel ${facts['kernel']}")
+    }
+  }
 }
