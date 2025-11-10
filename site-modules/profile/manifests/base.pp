ab56718b162711268832e5e17f6cc347dfc88f76
trips4 <dennis.triplo@perforce.comm>
2025-11-10 16:35:43 -0600

    configured eyaml

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -8,8 +8,8 @@
 #  The password for the user (Sensitive).
 #
 class profile::base (
+  Sensitive[String] $password,
   String $username = 'test_user',
-  String $password = 'MyPassword123!',
 ) {
   case $facts['kernel'] {
     'Linux': {
