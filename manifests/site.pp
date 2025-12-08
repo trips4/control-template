6c311c1b0fcb9cbfd9748b724f8068584d79731a
trips4 <dennis.triplo@perforce.comm>
2025-12-08 14:37:47 -0600

    fixing stuf

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,18 +25,16 @@ File { backup => false }
 #
 # For more on node definitions, see: https://puppet.com/docs/puppet/latest/lang_node_definitions.html
 node default {
-  # Find the first instance of `classes` in hiera data and includes unique values. Does not merge results.
-  $classes = lookup('classes', Variant[String,Array[String]])
-  case $classes {
-    String[1]: {
-      include $classes
-    }
-    Array[String[1],1]: {
-      $classes.unique.include
-    }
-    default: {
-      notify { 'No classes found in hiera data': }
-      #fail('This node did not receive any classification')
-    }
+  # Use Optional type and default to [] if not found
+  $classes = lookup('classes', Optional[Variant[String, Array[String]]], 'first', undef)
+
+  if $classes =~ String[1] {
+    include $classes
+  } elsif $classes =~ Array[String[1], 1] {
+    $classes.unique.include
+  } else {
+    notify { 'No classes found in hiera data': }
+    # Optionally fail here if you want to enforce classification
+    # fail('This node did not receive any classification')
   }
 }
