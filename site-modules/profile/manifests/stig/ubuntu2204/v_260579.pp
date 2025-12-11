8213c5ff3a2299e4f53f34102e53d5b772337a37
trips4 <dennis.triplo@perforce.comm>
2025-12-11 15:53:15 -0600

    new profile for 260579

---
+++ site-modules/profile/manifests/stig/ubuntu2204/v_260579.pp
@@ -0,0 +1,30 @@
+class profile::stig::ubuntu2204::v_260579 (
+  Boolean $control_enable = true,
+) {
+  if $control_enable {
+    file { '/etc/pam_pkcs11/':
+      ensure => directory,
+      mode   => '0755',
+      owner  => 'root',
+      group  => 'root',
+    }
+
+    file { '/etc/pam_pkcs11/pam_pkcs11.conf':
+      ensure  => file,
+      mode    => '0644',
+      owner   => 'root',
+      group   => 'root',
+      require => File['/etc/pam_pkcs11/'],
+      content => @(END)
+        # Minimal pam_pkcs11 configuration for STIG V-260579 compliance
+        # This file must not be empty.
+        use_pkcs11_module = opensc;
+        pam_pkcs11 {
+          debug = false;
+        }
+        | END
+    }
+  } else {
+    notify { 'STIG V-260579 control is disabled, no changes made.': }
+  }
+}
