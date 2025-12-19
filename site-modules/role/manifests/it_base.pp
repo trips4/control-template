a36d82a362ce14451f87e81d3955647040c39b19
trips4 <dennis.triplo@perforce.comm>
2025-12-19 13:33:06 -0600

    New IT Base Role

---
+++ site-modules/role/manifests/it_base.pp
@@ -0,0 +1,6 @@
+# This role would be made of all the profiles that need to be included to make a database server work
+# All roles should include the base profile
+class role::it_base {
+  include profile::base
+  include comply
+}
