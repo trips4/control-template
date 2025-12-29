7e34bc8bf89ace1065e59a492b69061309894f6e
trips4 <dennis.triplo@perforce.comm>
2025-12-29 15:04:13 -0600

    update

--- site-modules/profile/manifests/sudo_users.pp
+++ site-modules/profile/manifests/sudo_users.pp
@@ -1,14 +1,18 @@
+
 class profile::sudo_users (
-  Hash $users
+  Hash $users,
 ) {
-# Determine the appropriate admin group based on OS family
+  # Determine the appropriate admin group based on OS family
   $admin_group = $facts['os']['family'] ? {
     'RedHat' => 'wheel',
     default  => 'sudo',
   }
 
-# Merge the admin group into each user definition
-  $users_with_group = $users.map |$username, $attributes| {
-    $username => merge($attributes, { 'groups' => [$admin_group] })
+  # Merge the admin group into each user definition
+  $users_with_group = $users.transform_values |$attributes| {
+    merge($attributes, { 'groups' => [$admin_group] })
   }
+
+  # Create user resources
+  create_resources('user', $users_with_group)
 }
