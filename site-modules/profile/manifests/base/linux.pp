91267f05708720282fec8b9fd911383bd3814062
trips4 <dennis.triplo@perforce.comm>
2025-12-29 14:43:23 -0600

    finish up

--- site-modules/profile/manifests/base/linux.pp
+++ site-modules/profile/manifests/base/linux.pp
@@ -2,6 +2,6 @@ class profile::base::linux {
   package { ['vim', 'curl']:
     ensure => installed,
   }
-  #include profile::sudo_users
+  include profile::sudo_users
   #include profile::ssh_config
 }
