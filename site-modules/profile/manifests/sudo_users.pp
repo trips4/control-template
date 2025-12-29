f275d28cab603b76a4fad6883adfae19c445e7f7
trips4 <dennis.triplo@perforce.comm>
2025-12-29 15:41:38 -0600

    update

--- site-modules/profile/manifests/sudo_users.pp
+++ site-modules/profile/manifests/sudo_users.pp
@@ -19,24 +19,31 @@
 #   create_resources('user', $users_with_group)
 # }
 
+
 class profile::sudo_users (
   Hash $users,
 ) {
+  # Determine the appropriate admin group based on OS family
   $admin_group = $facts['os']['family'] ? {
     'RedHat' => 'wheel',
     default  => 'sudo',
   }
 
+  # Build a new hash with Sensitive password and correct group
   $users_with_group = $users.reduce({}) |$memo, $item| {
     $username   = $item[0]
     $attributes = $item[1]
 
-    # Wrap password in Sensitive type if present
+    # Wrap password in Sensitive if present
+    $updated_attributes = $attributes
     if $attributes['password'] {
-      $attributes['password'] = Sensitive($attributes['password'])
+      $updated_attributes = merge($attributes, { 'password' => Sensitive($attributes['password']) })
     }
 
-    merge($memo, { $username => merge($attributes, { 'groups' => [$admin_group] }) })
+    # Merge group info
+    $final_attributes = merge($updated_attributes, { 'groups' => [$admin_group] })
+
+    merge($memo, { $username => $final_attributes })
   }
 
   create_resources('user', $users_with_group)
