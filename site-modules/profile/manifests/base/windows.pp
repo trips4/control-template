edc2a621c38e6660d5b79c51e98c6e75bf09232c
trips4 <dennis.triplo@perforce.comm>
2025-12-30 14:40:50 -0600

    added dsc to puppetfile

--- site-modules/profile/manifests/base/windows.pp
+++ site-modules/profile/manifests/base/windows.pp
@@ -4,4 +4,12 @@ class profile::base::windows {
     ensure   => installed,
     provider => 'chocolatey',
   }
+  dsc_resource { 'example_file':
+    adapter    => 'Microsoft.Windows/WindowsPowerShell',
+    type       => 'PSDesiredStateConfiguration/File',
+    properties => {
+      'DestinationPath' => 'C:/test.txt',
+      'Contents'        => 'Hello from Puppet!',
+    },
+  }
 }
