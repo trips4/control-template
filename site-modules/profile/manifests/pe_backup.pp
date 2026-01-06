820e84c08f140e7a8f6f4dc02911e25f6229e375
trips4 <dennis.triplo@perforce.comm>
2026-01-06 12:21:56 -0600

    replaced exec resource

--- site-modules/profile/manifests/pe_backup.pp
+++ site-modules/profile/manifests/pe_backup.pp
@@ -3,28 +3,28 @@ class profile::pe_backup {
   # This is a placeholder class for PE Backup related configurations.
   # You can add resources and configurations here as needed.
   #
+  $backup_script_path = '/usr/local/bin/pe_backup.sh'
+  $cron_user          = 'root'
+  $backup_directory   = '/var/puppetlabs/backups'
+
   file { 'puppet backup script':
     ensure  => 'file',
-    path    => '/usr/local/bin/pe_backup.sh',
+    path    => $backup_script_path,
     content => template('profile/pe_backup.epp'),
     mode    => '0755',
   }
 
-  schedule { 'daily pe backup':
-    range  => '13 - 22',
-    period => daily,
-    repeat => 1,
-  }
-
-  exec { 'run pe backup':
-    command  => '/usr/local/bin/pe_backup.sh',
-    schedule => 'daily pe backup',
-    require  => File['puppet backup script'],
-    timeout  => 600,
+  cron { 'puppet_backup':
+    ensure  => present,
+    command => $backup_script_path,
+    user    => $cron_user,
+    minute  => '0',
+    hour    => '2',
+    weekday => '5',
   }
 
   tidy { 'cleanup pe backup':
-    path    => '/var/puppetlabs/backups',
+    path    => $backup_directory,
     age     => '1w',
     recurse => true,
     matches => ['pe-backup-*.tgz', 'orchestration-services-backup-*.tgz', 'console-services-backup-*.tgz'],
