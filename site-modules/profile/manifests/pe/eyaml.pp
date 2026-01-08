b4f0539a728efd874af0e035dc448ad5462e5246
trips4 <dennis.triplo@perforce.comm>
2026-01-08 09:39:47 -0600

    Puppet eyaml Setup

--- site-modules/profile/manifests/pe/eyaml.pp
+++ site-modules/profile/manifests/pe/eyaml.pp
@@ -4,7 +4,8 @@ class profile::pe::eyaml {
   }
   file { '/etc/eyaml/config.yaml':
     ensure  => 'file',
-    content => 'pkcs7_private_key: "/etc/puppetlabs/puppet/eyaml/keys/private_key.pkcs7.pem"',
+    content => 'pkcs7_private_key: "/etc/puppetlabs/puppet/eyaml/keys/private_key.pkcs7.pem"\n
+                pkcs7_public_key:  "/etc/puppetlabs/puppet/eyaml/keys/public_key.pkcs7.pem"',
     require => File['/etc/eyaml'],
   }
   file { '/etc/puppetlabs/puppet/eyaml':
