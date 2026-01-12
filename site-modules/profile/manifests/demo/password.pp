5e04e192932135d176f4688611e04ea6be8a3164
trips4 <dennis.triplo@perforce.comm>
2026-01-12 11:17:58 -0600

    password demo

--- site-modules/profile/manifests/demo/password.pp
+++ site-modules/profile/manifests/demo/password.pp
@@ -5,4 +5,8 @@ String $encrypted_pw) {
     ensure  => 'file',
     content => "This is the plain text password ${plain_text_pw}\nThis is the encrypted password ${encrypted_pw}",
   }
+  file { '/tmp/password2.txt':
+    ensure  => 'file',
+    content => "This is the plain text password ${plain_text_pw}\nThis is the encrypted password ${encrypted_pw}",
+  }
 }
