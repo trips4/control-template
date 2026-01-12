0eaded08a572f53dcd1aaf8d5efd6a294a7eab98
trips4 <dennis.triplo@perforce.comm>
2026-01-12 11:36:05 -0600

    add sensitive pw

--- site-modules/profile/manifests/demo/password.pp
+++ site-modules/profile/manifests/demo/password.pp
@@ -1,6 +1,6 @@
 class profile::demo::password (
   String $plain_text_pw,
-String $encrypted_pw) {
+Sensitive[String] $encrypted_pw) {
   file { '/tmp/password.txt':
     ensure  => 'file',
     content => "This is the plain text password ${plain_text_pw}\nThis is the encrypted password ${encrypted_pw}",
