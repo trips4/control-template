0ecc31cf430a69d2833c9f2185b3297790d74fc0
trips4 <dennis.triplo@perforce.comm>
2026-01-12 13:47:51 -0600

    webserver demo

--- site-modules/profile/manifests/demo/webserver.pp
+++ site-modules/profile/manifests/demo/webserver.pp
@@ -1,20 +1,17 @@
 class profile::demo::webserver {
+  package { 'apache2':
+    ensure => installed,
+  }
 
-  class demo::apache {
-    package { 'apache2':
-      ensure => installed,
-    }
+  file { '/var/www/html/index.html':
+    ensure  => file,
+    content => 'Hello from Puppet!',
+    require => Package['apache2'],
+  }
 
-    file { '/var/www/html/index.html':
-      ensure  => file,
-      content => 'Hello from Puppet!',
-      require => Package['apache2'],
-    }
-
-    service { 'apache2':
-      ensure    => running,
-      enable    => true,
-      subscribe => File['/var/www/html/index.html'],
-    }
+  service { 'apache2':
+    ensure    => running,
+    enable    => true,
+    subscribe => File['/var/www/html/index.html'],
   }
 }
