043b9779c47976a05bc61831c25a381415dca93c
trips4 <dennis.triplo@perforce.comm>
2026-01-12 14:45:31 -0600

    update

--- site-modules/profile/manifests/demo/password.pp
+++ site-modules/profile/manifests/demo/password.pp
@@ -1,11 +1,20 @@
 class profile::demo::password (
-String $plain_text_pw,
-Sensitive[String] $encrypted_pw,
-String $encrypted_pw_nosens){
+  String $plain_text_pw,
+  Sensitive[String] $encrypted_pw,
+String $encrypted_pw_nosens) {
+  # file { '/tmp/password.txt':
+  #   ensure  => 'file',
+  #   content => "This is the plain text password ${plain_text_pw}\n
+  #   This is the encrypted password ${encrypted_pw}\n
+  #   This is the encrypted password not protected with sensitive ${encrypted_pw_nosens}",
+  # }
+
   file { '/tmp/password.txt':
     ensure  => 'file',
-    content => "This is the plain text password ${plain_text_pw}\n
-    This is the encrypted password ${encrypted_pw}\n
-    This is the encrypted password not protected with sensitive ${encrypted_pw_nosens}",
+    content => @(END),
+    This is the plain text password ${plain_text_pw}
+    This is the encrypted password ${encrypted_pw}
+    This is the encrypted password not protected with sensitive ${encrypted_pw_nosens}
+    | END
   }
 }
