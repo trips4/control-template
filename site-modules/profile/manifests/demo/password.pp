36475b5c12c584984a9fdba9375f4f1de03b0c50
trips4 <dennis.triplo@perforce.comm>
2026-01-12 14:51:39 -0600

    test

--- site-modules/profile/manifests/demo/password.pp
+++ site-modules/profile/manifests/demo/password.pp
@@ -12,9 +12,9 @@ String $encrypted_pw_nosens) {
   file { '/tmp/password.txt':
     ensure  => 'file',
     content => "@(END),
-    This is the plain text password ${plain_text_pw}
-    This is the encrypted password ${encrypted_pw}
-    This is the encrypted password not protected with sensitive ${encrypted_pw_nosens}
+    This is plain text ${plain_text_pw}
+    This is encrypted and sensitive ${encrypted_pw}
+    This is the encrypted not flagged sensitive ${encrypted_pw_nosens}
     | END",
   }
 }
