9da9aefab3969025a1fde0395a5fcbfdc2c9aa6f
trips4 <dennis.triplo@perforce.comm>
2026-01-16 11:18:19 -0600

    update password

--- site-modules/profile/manifests/sudo_users.pp
+++ site-modules/profile/manifests/sudo_users.pp
@@ -1,3 +1,14 @@
+##
+# @summary
+# Manage sudo users with secure password handling and OS-specific admin group assignment.
+#
+# This class creates user accounts with sudo (or wheel) group membership, securely hashes passwords, and wraps them in Sensitive.
+#
+# @param users
+#   A hash of user definitions. Each key is a username, and each value is a hash of user attributes (including 'password').
+#   Passwords are hashed using SHA-512 and wrapped in Sensitive for security.
+##
+
 class profile::sudo_users (
   Hash $users,
 ) {
@@ -7,17 +18,15 @@ class profile::sudo_users (
     default  => 'sudo',
   }
 
-  # Build a new hash with Sensitive password and correct group
-  $users_with_group = $users.reduce({}) |$memo, $item| {
-    $username   = $item[0]
-    $attributes = $item[1]
-
+  # Transform the users hash with proper group and Sensitive password
+  $users_with_group = $users.reduce({}) |$memo, $username, $attributes| {
+    # Hash the password if provided
     $hashed_password = $attributes['password'] ? {
       undef   => undef,
       default => pw_hash($attributes['password'], 'SHA-512', 'myrandomstring'),
     }
 
-    # If password exists, wrap it in Sensitive
+    # Merge original attributes with group and Sensitive password
     $final_attributes = merge($attributes, {
         'groups'   => [$admin_group],
         'password' => $hashed_password ? {
@@ -26,8 +35,10 @@ class profile::sudo_users (
         }
     })
 
+    # Add this user to the accumulator hash
     merge($memo, { $username => $final_attributes })
   }
 
+  # Create user resources dynamically
   create_resources('user', $users_with_group)
 }
