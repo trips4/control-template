3d1c252ca5f5a9f4e4091b303dcbd7f954185e3f
trips4 <dennis.triplo@perforce.comm>
2026-01-29 14:31:17 -0600

    update

--- site-modules/profile/manifests/demo/password.pp
+++ site-modules/profile/manifests/demo/password.pp
@@ -22,14 +22,18 @@ class profile::demo::password (
   }
 
   file_line { 'Password 1':
-    ensure => present,
-    path   => $file,
-    line   => "This password ${encrypted_password} was encrypted and added tov hiera. During compilation it was decrypted and assigned to 'encrypted_password'/n ",
+    ensure  => present,
+    path    => $file,
+    line    => "This password ${encrypted_password} was encrypted and added tov hiera. During compilation it was decrypted and assigned to 'encrypted_password'/n ",
+    match   => "^${encrypted_password}",
+    replace => true,
   }
 
   file_line { 'Password 2':
-    ensure => present,
-    path   => $file,
-    line   => "This password ${encrypted_pw} was encrypted and added to hiera. Our class parameter was typed to sensitive, requiring us to assign it as such in hiera",
+    ensure  => present,
+    path    => $file,
+    line    => "This password ${encrypted_pw} was encrypted and added to hiera. Our class parameter was typed to sensitive, requiring us to assign it as such in hiera",
+    match   => "^${encrypted_pw}",
+    replace => true,
   }
 }
