cec3824280b7d4c1e997a90d95ad37981afc2196
trips4 <dennis.triplo@perforce.comm>
2026-01-29 16:25:56 -0600

    update

--- site-modules/profile/manifests/demo/secrets.pp
+++ site-modules/profile/manifests/demo/secrets.pp
@@ -11,7 +11,7 @@
 #     encrypted_pw          => Sensitive('encryptedValue'),
 #     encrypted_password   => 'encryptedValue',
 #   }
-class profile::demo::password (
+class profile::demo::secrets (
   # BAD: plain String password (decrypted at compile time, stored in catalog)
   String $insecure_password,
 
